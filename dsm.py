99a06e2907187e5fa8440b53c33f5a266b64db73
Davidraja777 <87680842+Davidraja777@users.noreply.github.com>
2023-09-07 11:54:36 +0800

    Update dsm.py

--- dsm.py
+++ dsm.py
@@ -117,78 +117,87 @@ while True:
             sg.popup('Record Added Successfully!', keep_on_top=True)
 
     if event == 'View Records':
-        # Get the full date from the user
-        date_selected = sg.popup_get_date(title="Select Date", no_titlebar=True)
-        if date_selected:
-            month, day, year = date_selected
-            filename = f"records_{month}_{year}.json"
-
-        else:
+        # Get the start date from the user
+        start_date_selected = sg.popup_get_date(title="Select Start Date", no_titlebar=True)
+        if not start_date_selected:
             continue
 
+        # Get the end date from the user
+        end_date_selected = sg.popup_get_date(title="Select End Date", no_titlebar=True)
+        if not end_date_selected:
+            continue
+
+        start_month, start_day, start_year = start_date_selected
+        end_month, end_day, end_year = end_date_selected
+    
+        start_date_str = datetime(start_year, start_month, start_day).strftime("%Y-%m-%d")
+        end_date_str = datetime(end_year, end_month, end_day).strftime("%Y-%m-%d")
+    
         # Load records from the selected month's file
-        try:
-            with open(filename, 'r') as file:
-                records = json.load(file)
-        except FileNotFoundError:
-            sg.popup_error(f"No records found for {calendar.month_name[month]} {day} {year}!", keep_on_top=True)
-            records = []
-        except json.JSONDecodeError:
-            sg.popup_error(f"Error reading records for {calendar.month_name[month]} {day} {year}!", keep_on_top=True)
-            records = []
+        records = []
+        for year in range(start_year, end_year + 1):
+            for month in range(start_month if year == start_year else 1, end_month + 1 if year == end_year else 13):
+                filename = f"records_{month}_{year}.json"
+                try:
+                    with open(filename, 'r') as file:
+                        month_records = json.load(file)
+                        records.extend(month_records)
+                except FileNotFoundError:
+                    continue
+                except json.JSONDecodeError:
+                    sg.popup_error(f"Error reading records for {calendar.month_name[month]} {year}!", keep_on_top=True)
+                    continue
 
-        date_str = datetime(year, month, day).strftime("%Y-%m-%d")
-        records = [record for record in records if record[0].startswith(date_str)]
+    # Filter records based on the selected date range
+    records = [record for record in records if start_date_str <= record[0].split(" ")[0] <= end_date_str]
 
-        headers = ['Date', 'Created By', 'Delivery Group', 'TU Created', 'Running Number', 'Check Out', 'Updated By', 'Time Updated']
-        data = [[str(r[i]) for i in range(len(r))] for r in records]
-        view_layout = [
-            [sg.Table(values=data, headings=headers, display_row_numbers=False,
-                auto_size_columns=True, num_rows=min(10, len(data)),
-                justification='center', key='-TABLE-', enable_events=True,
-                bind_return_key=True, select_mode=sg.TABLE_SELECT_MODE_EXTENDED)],
-            [sg.Button('Edit', disabled=True), sg.Button('Select All'), sg.Button('Save To Excel'), sg.Button('Close')]
-        ]
+    headers = ['Date', 'Created By', 'Delivery Group', 'TU Created', 'Running Number', 'Check Out', 'Updated By', 'Time Updated']
+    data = [[str(r[i]) for i in range(len(r))] for r in records]
+    view_layout = [
+        [sg.Table(values=data, headings=headers, display_row_numbers=False,
+            auto_size_columns=True, num_rows=min(10, len(data)),
+            justification='center', key='-TABLE-', enable_events=True,
+            bind_return_key=True, select_mode=sg.TABLE_SELECT_MODE_EXTENDED)],
+        [sg.Button('Edit', disabled=True), sg.Button('Select All'), sg.Button('Save To Excel'), sg.Button('Close')]
+    ]
 
+    if view_window:
+        view_window.close()
+    view_window = sg.Window('View Records', view_layout, keep_on_top=True, finalize=True)
 
-        if view_window:
+    while view_window:
+        view_event, view_values = view_window.read()
+
+        if view_event == sg.WINDOW_CLOSED or view_event == 'Close':
             view_window.close()
-        view_window = sg.Window('View Records', view_layout, keep_on_top=True, finalize=True)
+            view_window = None
+            selected_record_to_edit = None
+            break
 
-        while view_window:
-            view_event, view_values = view_window.read()
+        if view_event == '-TABLE-':
+            # If a record was selected
+            selected_record_to_edit = view_values['-TABLE-'][0]
+            view_window['Edit'].update(disabled=False)
 
-            if view_event == sg.WINDOW_CLOSED or view_event == 'Close':
-                view_window.close()
-                view_window = None
-                selected_record_to_edit = None
-                break
-
-            if view_event == '-TABLE-':
-                # If a record was selected
-                selected_record_to_edit = view_values['-TABLE-'][0]
-                view_window['Edit'].update(disabled=False)
-
-            if view_event == 'Edit' and view_values['-TABLE-']:
-                selected_record_to_edit = view_values['-TABLE-'][0]
-                record_to_edit = records[selected_record_to_edit]
-                window['-DELIVERY_GROUP-'].update(record_to_edit[2])
-                view_window.close()
-                view_window = None
-                selected_record_to_edit = None
-
-            if view_event == 'Select All':
-                view_window['-TABLE-'].update(select_rows=[i for i in range(len(data))])
-                view_window['Edit'].update(disabled=False)
-
-            if view_event == 'Save To Excel':
-                filename = sg.popup_get_file('Save To Excel', save_as=True, default_extension=".xlsx", no_window=True, file_types=(("Excel Files", "*.xlsx"), ("All Files", "*.*")), keep_on_top=True)
-                if filename:
-                    save_to_excel(records, filename)
-                    sg.popup('Records Saved Successfully!', keep_on_top=True)
-        if view_window:
+        if view_event == 'Edit' and view_values['-TABLE-']:
+            selected_record_to_edit = view_values['-TABLE-'][0]
+            record_to_edit = records[selected_record_to_edit]
+            window['-DELIVERY_GROUP-'].update(record_to_edit[2])
             view_window.close()
+            view_window = None
+            selected_record_to_edit = None
 
+        if view_event == 'Select All':
+            view_window['-TABLE-'].update(select_rows=[i for i in range(len(data))])
+            view_window['Edit'].update(disabled=False)
+
+        if view_event == 'Save To Excel':
+            filename = sg.popup_get_file('Save To Excel', save_as=True, default_extension=".xlsx", no_window=True, file_types=(("Excel Files", "*.xlsx"), ("All Files", "*.*")), keep_on_top=True)
+            if filename:
+                save_to_excel(records, filename)
+                sg.popup('Records Saved Successfully!', keep_on_top=True)
+    if view_window:
+        view_window.close()
 
     if event == 'Update Record':
         # If the user is allowed to update a record
